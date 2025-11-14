8d1678e5e630622ca4a76721143c5816e2b19cf5
Kanashii-Kito <ERicc468@nycstudents.net>
2025-11-14 09:47:07 -0500

    Update t.py

--- t.py
+++ t.py
@@ -59,4 +59,23 @@ Jerry = Hero("Jerry", 0, ["#BlameJerry"])
 Jerry.buy({"title": "Broom", "atk": -1})
 print(Jerry.__dict__)
 
+# class Pet:
     
+
+
+
+
+
+
+    
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  # double underscore means "private"
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
