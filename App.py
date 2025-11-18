8bad6aae89beb10d5bc074efff5e1f9a8e905779
Kanashii-Kito <ERicc468@nycstudents.net>
2025-11-18 10:10:21 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,26 @@
-def login_valid (email, password):
-    email = input ("Please enter a email")
-    error_letters = "@"
-    if email == "@":
+# def login_valid (email, password):
+#     targetchar = "@"
+#     if not email:
+#         return "Please enter a email."
+#     if email == " ":
+#         return "Please enter a email without spaces in it"
+#     if email.islower == 
+
+#     password = input ("Please enter ur password")
         
+
+
+
+def isvalid (email, password):
+    if "@" not in email:
+        return "Not vaild email"
+    if " " in email:
+        return "Spaces are not allowed in a email."
+    char = len(password)
+    if char > 8:
+        return "You need at least 8 Characters in ur password."
+
+
+    return {'Email':email, 'Password': password}
+
+print(isvalid("test", "test"))
