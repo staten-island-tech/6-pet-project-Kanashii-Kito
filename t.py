824ccdf7ed14495bfda308fd1d5748d53f745947
Kanashii-Kito <ERicc468@nycstudents.net>
2025-11-20 10:17:08 -0500

    Update t.py

--- t.py
+++ t.py
@@ -1,14 +1,52 @@
 
 
 class Hero:
-    def __init__(self, name, money, inventory):
-        self.name = name
-        self.money = money
+    def __init__(pet, happiness, hunger, hygiene, energy):
+        pet.happiness = happiness
+        pet.hunger = hunger  
+        pet.hygiene = hygiene
+        pet.energy = energy
+    
+    def buy (self, shop, inventory):
+        self.shop = shop
         self.inventory = inventory
+    
+    shop = [
+{
+    "Name": "Pet Food",
+    "Cost": 2499.99,
+    "Department": "Pet", 
+    "Description": "Packages From The Depths Hell.",
+},
 
-    def buy(self, item):
-        self.inventory.append(item)
-        print(self.inventory)
+{
+    "Name": "Premium Pet Food",
+    "Cost": 4999.99,
+    "Department": "Pet", 
+    "Description": "Faster Delivery of Packages From The Depths Hell.",
+},
+
+{
+    "Name": "Treats",
+    "Cost": 19999.99,
+    "Department": "Pet", 
+    "Description": "Pet Treats.",
+},
+
+{
+    "Name": "Pet Insurance",
+    "Cost": 99999.99,
+    "Department": "Pet", 
+    "Description": "Insurance for your Pet."
+},
+
+{
+    "Name": "Contract || Contract",
+    "Cost": 0.00,
+    "Department": "NDC-M", 
+    "Description": "N/A."
+},
+]
 
 
 
