59c3822973ffdc7dcc863c82e0b60c5cd2768bd8
Kanashii-Kito <ERicc468@nycstudents.net>
2025-11-20 09:52:24 -0500

    45

---
+++ EMAILpassword.py
@@ -0,0 +1,83 @@
+# def login_valid (email, password):
+#     targetchar = "@"
+#     if not email:
+#         return "Please enter a email."
+#     if email == " ":
+#         return "Please enter a email without spaces in it"
+#     if email.islower == 
+
+#     password = input ("Please enter ur password")
+        
+
+
+
+""" def isvalid (email, password):
+    if "@" not in email:
+        return "Not vaild email"
+    if " " in email:
+        return "Spaces are not allowed in a email."
+    char = len(password)
+    if char < 8:
+        return "You need at least 8 Characters in ur password."
+    # if password.isupper:
+    #     return ""
+    # the commented lines of code above won't work because .upper is for the whole string to be uppercased.
+
+
+
+    if (any(x.isupper() for x in password) and len < 0):
+        return "Please put at least 1 uppercase letter in ur password."
+    
+    if (any(x.isdigit() for x in password) and len < 0):
+        return "Please have at least 1 number in ur password"
+
+
+
+
+    return {'Email':email, 'Password': password}
+
+print(isvalid("test@gmail.com", "test1236")) """
+
+
+
+
+
+
+
+def isvalid(email, password):
+
+    if "@" not in email:
+        return "Not a vaild Email"
+    
+    if not isinstance(email, str) or not isinstance(password, str):
+        return "Error: Email and Passwords must be strings."
+    
+    if " " in password:
+        return "Spaces are not allowed in an Password."
+
+    if " " in email:
+        return "Spaces are not allowed in an email."
+
+    if len(password) < 8:
+        return "You need at least 8 characters in your password."
+
+    if not any(x.isupper() for x in password):
+        return "Please put at least 1 uppercase letter in your password."
+
+    if not any(x.isdigit() for x in password):
+        return "Please have at least 1 number in your password."
+
+    return {'Email': email, 'Password': password}
+
+print(isvalid("test@gmail.com", "Testeeee1@lkjsdflj"))
+
+
+
+
+
+
+
+
+
+
+
--- t.py
+++ t.py
@@ -1,7 +1,14 @@
 
 
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
 
-
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
 
 
 
